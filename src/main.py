7df5d17362a25f8cfa8d63fda24ac987a3a08dfb
Charles-1414 <me@charles14.xyz>
2022-04-15 21:03:41 +0800

    v1.18c2 Bug fixes of v1.18c

--- src/main.py
+++ src/main.py
@@ -15,6 +15,10 @@ import traditional.main
 import globaltrucking.main
 import truckersmp.main
 from truckersmp.tmp import UpdateTMPMap, UpdateTMPTraffic
+from datetime import datetime
+
+from db import newconn
+import time
 
 import threading
 
@@ -27,11 +31,21 @@ async def on_ready():
 async def on_ready():
     print(f"[Traditional Bot] Logged in as {tbot.user} (ID: {tbot.user.id})")
 
+def ClearVCMonthly():
+    while 1:
+        if datetime.today().day == 1:
+            conn = newconn()
+            cur = conn.cursor()
+            cur.execute(f"UPDATE vcrecord SET minutes = 0")
+            conn.commit()
+        time.sleep(43200)
+
 config_txt = open("./bot.conf","r").read()
 config = json.loads(config_txt)
 
 threading.Thread(target=UpdateTMPMap).start()
 threading.Thread(target=UpdateTMPTraffic).start()
+threading.Thread(target=ClearVCMonthly).start()
 loop = asyncio.get_event_loop()
 loop.create_task(bot.start(config["token"]))
 loop.create_task(tbot.start(config["token"]))
